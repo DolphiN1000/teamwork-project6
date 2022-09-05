f59df5efb7f287e078a682615acf18dae6546e88
saniyo <saniyo.ua@gmail.com>
2022-09-05 23:23:14 +0300

    add mobile-desk-tablet menu buy now

--- src/js/anchor-logic-mobile.js
+++ src/js/anchor-logic-mobile.js
@@ -4,3 +4,18 @@ function closeMobileMenu() {
      };
     refs.menu.classList.remove('is-open');
 };
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-buy-now-open]'),
+    closeModalBtn: document.querySelector('[data-modal-buy-now-close]'),
+    modal: document.querySelector('[data-modal-buy-now]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
