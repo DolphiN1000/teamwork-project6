ec846f0cd781bd19820ba35bae7b3851d29704c1
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-06 19:42:47 +0000

    Deploying to gh-pages from @ DolphiN1000/teamwork-project6@d288f9e81c87f2f1183877cc9cd20021ca59152e 🚀

--- index.85071f63.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-products-open2]"),closeModalBtn:document.querySelector("[data-modal-products-close]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.85071f63.js.map
