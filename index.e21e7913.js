2a24311a6aec7185616084c71b63bf7414992f44
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-06 20:12:21 +0000

    Deploying to gh-pages from @ DolphiN1000/teamwork-project6@0ebe22f6070d74838f5065c3b2bbefd6ce73c0fc 🚀

--- index.e21e7913.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-products-open1]"),closeModalBtn:document.querySelector("[data-modal-products-close]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.e21e7913.js.map
