2ccd45cf40be98eb0a3dd8c57ca8b41889f0b49e
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-07 05:15:01 +0300

    fixing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './js/swiper';
 import './js/slider';
 import './js/modal';
 import './js/mobile-menu.js';
-
+import './js/data-menu';
+import './js/anchor-logic-mobile';
 import './js/modal-location';
 import './js/modal-franchise';
 import './js/read-more';
+import './js/modal-products1';
+import './js/modal-products2';
+import './js/modal-products3';
 
 
